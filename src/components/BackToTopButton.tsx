ffdd0a59c43fd77eab927fa658149b30b773176d
shanky-ced <shashankrai2022jun@cedcoss.com>
2023-07-01 17:15:51 +0530

    added back to top component

--- src/components/BackToTopButton.tsx
+++ src/components/BackToTopButton.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Fab } from "@mui/material";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import React, { useEffect, useState,useCallback } from "react";
 
 const BackToTopButton = () => {
-  return (
-    <div>BackToTopButton</div>
-  )
-}
+	const [show, setShow] = useState(false);
 
-export default BackToTopButton
+  const handleClick = useCallback(()=>{
+    window.scrollTo({top:0})
+  },[window])
+
+	useEffect(() => {
+		window.addEventListener("scroll", () => {
+			if (window.screenX > 1000) {
+				setShow(true);
+			} else {
+				setShow(false);
+			}
+		});
+		return () => {
+			window.removeEventListener("scroll", () => {});
+		};
+	}, [window]);
+
+	return (
+		<>
+			{show && (
+				<Fab disabled aria-label="like" onClick={handleClick}>
+					<KeyboardArrowUpIcon />
+				</Fab>
+			)}
+		</>
+	);
+};
+
+export default BackToTopButton;
